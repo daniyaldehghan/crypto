1ac9d5ce66b1d50f3ed1b0267ff28bc62b02dcc5
daniyaldehghan <daniyaldehghani.madise@gmail.com>
2024-10-20 19:13:15 +0330

    Add files via upload

---
+++ layout/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./styled.module.css" 
+
+function Header() {
+  return (
+    <div className={styles.header}>
+         <h1>Crypto-app</h1>
+         <p>react-js</p>
+    </div>
+  )
+}
+
+export default Header
